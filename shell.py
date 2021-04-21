72ab0b53e0db4f08493b86e39534db3550f89112
Omar-Torres11 <omar.torres11@upr.edu>
2021-04-20 23:18:44 -0400

    Update shell.py
    removed test parameter

--- shell.py
+++ shell.py
@@ -2,7 +2,7 @@ import Branch
 
 while True:
     text = input('Branch > ')
-    result, error = Branch.run('<stdin>',text)
+    result, error = Branch.run(text)
 
     if error: print (error.as_string())
     else: print(result)
